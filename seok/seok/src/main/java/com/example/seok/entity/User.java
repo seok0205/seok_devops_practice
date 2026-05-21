2be232abee72a4eacd549abbf9365292f65df14f
Jungseok Yu <9526yu@naver.com>
2026-05-22 04:09:43 +0900

    [BE] 기ë³] ] 로그인  API및 user entity 생성 ë°, 설정

---
+++ seok/seok/src/main/java/com/example/seok/entity/User.java
@@ -0,0 +1,27 @@
+package com.example.seok.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
