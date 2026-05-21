42f2064e2039084a1254d1898453b1d8d91b1437
Jungseok Yu <9526yu@naver.com>
2026-05-21 21:33:04 +0900

    [BE] sp initializeringboot í project & .gitignore

---
+++ seok/seok/src/main/java/com/example/seok/SeokApplication.java
@@ -0,0 +1,13 @@
+package com.example.seok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeokApplication.class, args);
+	}
+
+}
---
+++ seok/seok/src/test/java/com/example/seok/SeokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
